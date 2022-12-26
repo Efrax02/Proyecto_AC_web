274bdb4e6de1cf74149be21cdb4a826a44c8a1e3
EAH <EAH@spyro.es>
2022-12-26 12:09:11 +0100

    Clase de proveedores creada

---
+++ CompraComponentes/CompraComponentes/App_Code/Pedidos_Tienda.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CompraComponentes.App_Code
+{
+    public class Pedidos_Tienda
+    {
+        private int codPedido;
+        public int codPedido
+        {
+            get { return codPedido; }
+            set { codPedido = value; }
+        }
+
+        
+    }
+
+    public class Lineas_Pedidos_Tienda
+    {
+
+    }
+}
--- CompraComponentes/CompraComponentes/App_Code/Proveedores.cs
+++ CompraComponentes/CompraComponentes/App_Code/Proveedores.cs
@@ -7,6 +7,56 @@ namespace CompraComponentes.App_Code
 {
     public class Proveedores
     {
-        private int CodProveedor;
+        public Proveedores(int CodProveedor, 
+        string CodFiscalProveedor, 
+        string NombreProveedor, 
+        string Telefono, 
+        string Direccion, 
+        string Email)
+        {
+            codProveedor = CodProveedor;
+            codFiscalProveedor = CodFiscalProveedor;
+            nombreProveedor = NombreProveedor;
+            telefono = Telefono;
+            direccion = Direccion;
+            email = Email;
+        }
+
+        private int codProveedor;
+        public string CodProveedor
+        {
+            get{ return codProveedor;}
+            set{ codProveedor = value; }
+        }        
+        private string codFiscalProveedor;
+        public string CodFiscalProveedor
+        {
+            get { return codFiscalProveedor; }
+            set { codFiscalProveedor = value; }
+        }
+        private string nombreProveedor;
+        public string NombreProveedor
+        {
+            get { return nombreProveedor; }
+            set { nombreProveedor = value; }
+        }
+        private string telefono;
+        public string Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+        private string direccion;
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
     }
 }
